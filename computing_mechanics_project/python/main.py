49cdfbbf0ba2eb523a4fcea278e175a4c98fcaab
klikklakBEN_mob <borobol205@gmail.com>
2023-10-01 03:01:05 +0300

    modified return items for createNodes

--- computing_mechanics_project/python/main.py
+++ computing_mechanics_project/python/main.py
@@ -1,17 +1,37 @@
 import numpy as np
 
+#assuming there's a fucntion harvesting the node information from input txt/xls/spreadsheet file
+
+def checkEqNodes(elemCoord1, elemCoord2):
+
+    equalX = (elemCoord1[0] == elemCoord2[0])
+    equalY = (elemCoord1[1] == elemCoord2[1])
+
+    return (equalX & equalY)
+
 def createNodes(elemCoord):
     
     count = elemCoord.shape[0]
     nodeTable = np.zeros((count,3))
 
-    for i in range(count):
+    nodeTable[0][0] = 0
+    nodeTable[0][1] = elemCoord[i][0]
+    nodeTable[0][2] = elemCoord[i][1]
+    
+    i = 1
+    index_i = 1
+    while (i<count)
+        
+        i +=1
+        if checkEqNodes(elemCoord[i], elemCoord[i-1]:
+                        continue
         nodeTable[i][0] = i
         nodeTable[i][1] = elemCoord[i][0]
         nodeTable[i][2] = elemCoord[i][1]
+        
+        index_i += 1
 
-
-    return nodeTable
+    return nodeTable, index_i
 
 
 def createElem(nodes, nodeNum, matProp):
