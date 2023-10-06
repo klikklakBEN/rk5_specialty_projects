f9157403e5ab7763e94c2ca62873bf623044c70c
Plaklunk-klang <borobol205@gmail.com>
2023-10-06 19:34:24 +0300

    added form functions derivatives

--- computing_mechanics_project/python/main.py
+++ computing_mechanics_project/python/main.py
@@ -66,7 +66,42 @@ def N4(xi, eta):
 
     return 1./4 * (1 - xi) * (1 + eta);
 
+
+def dN1dxi(xi, eta):
+
+    return -1./4*(1 - eta)
+
+def dN1deta(xi, eta):
+
+    return -1./4*(1 - xi)
+
+def dN2dxi(xi, eta):
+
+    return 1./4*(1 - eta)
+
+
+def dN2deta(xi, eta):
+
+    return -1./4*(1 + xi)
+
+def dN3dxi(xi, eta):
+
+    return 1./4*(1 + eta)
+
+def dN3deta(xi, eta):
+
+    return 1./4*(1 + xi)
+
+def dN4dxi(xi, eta):
+
+    return -1./4*(1 + eta)
+
+def dN4deta(xi, eta):
+
+    return 1./4*(1 - xi)
+
 # define form function matrix
+
 
 def NMatrix(xi, eta):
 
@@ -83,6 +118,43 @@ def NMatrix(xi, eta):
 
     n_mtx[0][6] = N4(xi, eta)
     n_mtx[1][7] = N4(xi, eta)
+
+    return n_mtx;
+
+def dNdxiMatrix(xi, eta):
+
+    n_mtx = np.zeros((2,8))
+    
+    n_mtx[0][0] = dN1dxi(xi, eta)
+    n_mtx[1][1] = dN1dxi(xi, eta)
+ 
+    n_mtx[0][2] = dN2dxi(xi, eta)
+    n_mtx[1][3] = dN2dxi(xi, eta)
+   
+    n_mtx[0][4] = dN3dxi(xi, eta)
+    n_mtx[1][5] = dN3dxi(xi, eta)
+
+    n_mtx[0][6] = dN4dxi(xi, eta)
+    n_mtx[1][7] = dN4dxi(xi, eta)
+
+    return n_mtx;
+
+
+def dNdetaMatrix(xi, eta):
+
+    n_mtx = np.zeros((2,8))
+    
+    n_mtx[0][0] = dN1deta(xi, eta)
+    n_mtx[1][1] = dN1deta(xi, eta)
+ 
+    n_mtx[0][2] = dN2deta(xi, eta)
+    n_mtx[1][3] = dN2deta(xi, eta)
+   
+    n_mtx[0][4] = dN3deta(xi, eta)
+    n_mtx[1][5] = dN3deta(xi, eta)
+
+    n_mtx[0][6] = dN4deta(xi, eta)
+    n_mtx[1][7] = dN4deta(xi, eta)
 
     return n_mtx;
 
