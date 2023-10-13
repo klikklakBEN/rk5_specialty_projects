c5d74912541582832b34a967f73620d67ba33183
Plaklunk-klang <borobol205@gmail.com>
2023-10-13 16:57:12 +0300

    created input containers in main, removed obsolete functions in funcDefs

--- computing_mechanics_project/python/funcDefs.py
+++ computing_mechanics_project/python/funcDefs.py
@@ -2,52 +2,6 @@ import numpy as np
 
 #assuming there's a fucntion harvesting the node information from input txt/xls/spreadsheet file
 
-def checkEqNodes(node1, node2):
-
-    equalX = (node1[1] == node2[1])
-    equalY = (node1[2] == node2[2])
-
-    return (equalX & equalY);
-
-def createNodes(elemCoord):
-    
-    count = elemCoord.shape[0]
-    nodeTable = np.zeros((count,3))
-
-    nodeTable[0][0] = 0
-    nodeTable[0][1] = elemCoord[i][0]
-    nodeTable[0][2] = elemCoord[i][1]
-    
-    i = 1
-    index_i = 1
-    while (i<count):
-        
-        i +=1
-        if checkEqNodes(elemCoord[i], elemCoord[i-1]):
-            continue
-        nodeTable[i][0] = i
-        nodeTable[i][1] = elemCoord[i][0]
-        nodeTable[i][2] = elemCoord[i][1]
-        
-        index_i += 1
-
-    return nodeTable, index_i;
-
-
-def createElem(nodes, nodeNum, matProp):
-    
-    n1 = nodes[nodeNum[0]]
-    n2 = nodes[nodeNum[1]]
-    n3 = nodes[nodeNum[2]]
-    n4 = nodes[nodeNum[3]]
-
-    elemNodes = [n1, n2, n3, n4]
-    
-    elemE   = matProp[0]
-    elemPR  = matProp[1]
-
-    return elemNodes, elemE, elemPR;
-
 # defining form functions
 
 def N1(xi, eta):
@@ -158,12 +112,10 @@ def dNdetaMatrix(xi, eta):
 
     return n_mtx;
 
-def elemNormCoord(elem):
 
-    return;
+# Jacobian matrix
+def J(elemCoords, xi, eta):
 
-
-#tableOfNodes = txt.to.list.or.smth("ElemCoords")
-
+    return
 # make the damn program as easy as possible, do not make rocket science off of it!
 # if the file returns some data, adjust the code to be able to read it! 
--- computing_mechanics_project/python/main.py
+++ computing_mechanics_project/python/main.py
@@ -1,1 +1,41 @@
-import funcDefs
+import funcDefs as fd
+import numpy as np
+
+
+# node input file: 1st column - node number, 2nd-4th - x,y,z global coords
+nodeTable = np.loadtxt('nodeInput.txt')
+
+# elem input file: 1st column - elem number, 2nd-5th - 1st-4th nodes
+elemInputTable = np.loadtxt('elemInput.txt')
+elemInputCount = elemInputTable.shape[0]
+elemTable = np.zeros((elemInputCount, 5))
+
+matTable = np.loadtxt('matInput.txt')
+elemE    = matTable[0]
+elemPR   = matTable[1]
+
+for i in range(elemInputCount):
+
+    elemTable[i][0] = elemInputTable[i][0]
+    elemTable[i][1] = elemInputTable[i][6]
+    elemTable[i][2] = elemInputTable[i][7]
+    elemTable[i][3] = elemInputTable[i][8]
+    elemTable[i][4] = elemInputTable[i][9]
+
+elemData = np.zeros((elemInputCount, 13))
+
+for i in range(elemInputCount):
+
+    elemData[i][0] = elemTable[i][0]
+    elemData[i][1] = elemTable[i][1]
+    elemData[i][2] = elemTable[i][2]
+    elemData[i][3] = elemTable[i][3]
+    elemData[i][4] = elemTable[i][4]
+    elemData[i][5]  = nodeTable[int(elemTable[i][1]) - 1][1]
+    elemData[i][6]  = nodeTable[int(elemTable[i][1]) - 1][2]
+    elemData[i][7]  = nodeTable[int(elemTable[i][2]) - 1][1]
+    elemData[i][8]  = nodeTable[int(elemTable[i][2]) - 1][2]
+    elemData[i][9]  = nodeTable[int(elemTable[i][3]) - 1][1]
+    elemData[i][10] = nodeTable[int(elemTable[i][3]) - 1][2]
+    elemData[i][11] = nodeTable[int(elemTable[i][4]) - 1][1]
+    elemData[i][12] = nodeTable[int(elemTable[i][4]) - 1][2]
